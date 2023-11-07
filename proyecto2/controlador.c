d3ab1763a2ad0857328dbf17ff84026045dc525f
Replit user <>
2023-11-07 20:25:27 +0000

    cosas desde replit

---
+++ proyecto2/controlador.c
@@ -0,0 +1,14 @@
+/****************************************************************
+ * Fecha: X - Octubre - 2023
+ * Autor: Juan Sebastian Clavijo Martinez
+ * Tema:
+ * Objetivo:
+ ***************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  printf("Me encantan los pinguinos y su incapacidad para volar!\n");
+  return 0;
+}
